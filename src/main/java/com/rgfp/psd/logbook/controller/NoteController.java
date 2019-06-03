c685b6df53830436d3f5d6feedae59494561e0ec
Dagoberto Ceballos <dceballos@Falabella.cl>
2019-06-03 16:08:53 -0400

    config sonar

--- src/main/java/com/rgfp/psd/logbook/controller/NoteController.java
+++ src/main/java/com/rgfp/psd/logbook/controller/NoteController.java
@@ -1,8 +1,6 @@
 package com.rgfp.psd.logbook.controller;
 
 import com.rgfp.psd.logbook.domain.Note;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
